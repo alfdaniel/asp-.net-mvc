41a03a6e85df7dca27d4a2f3be830bd8300bbd53
Daniel Ferreira <daniel.alexandre22@hotmail.com>
2024-09-08 10:58:49 -0300

    filtros

--- ListasTarefasMVC/Controllers/HomeController.cs
+++ ListasTarefasMVC/Controllers/HomeController.cs
@@ -1,14 +1,67 @@
+using ListasTarefasMVC.Data;
 using ListasTarefasMVC.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace ListasTarefasMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+
+        public readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
         {
-            return View();
+
+            _context = context;
+        }
+
+        public IActionResult Index(string filtro)
+        {
+
+            var filtros = new Filtros(filtro);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Status = _context.Statuses.ToList();
+            ViewBag.Vencimento = Filtros.VencimentoValoresFiltro;
+
+
+            IQueryable<Tarefa> consulta = _context.Tarefas.Include(c => c.Categorias).Include(s => s.Status);
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(t => t.CategoriaNome == filtros.CategoriaNome);
+            }
+
+            if (filtros.TemStatus)
+            {
+                consulta = consulta.Where(t => t.StatusNome == filtros.StatusNome);
+            }
+
+            if (filtros.TemVencimento)
+            {
+                var hoje = DateTime.Today;
+
+                if (filtros.EPassado)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento < hoje);
+                }
+
+
+                if (filtros.EFuturo)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento > hoje);
+                }
+
+
+                if (filtros.EHoje)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento == hoje);
+                }
+            }
+
+            var tarefas = consulta.OrderBy(t => t.DataVencimento).ToList();
+
+            return View(tarefas);
         }
 
     }
--- ListasTarefasMVC/Models/Filtros.cs
+++ ListasTarefasMVC/Models/Filtros.cs
@@ -2,10 +2,41 @@
 {
     public class Filtros
     {
+        public Filtros(string filtroString)
+        {
+            // Se a string for nula, define o valor padrão "todos-todos-todos"
+            FiltroString = filtroString ?? "todos-todos-todos";
+            string[] filtros = FiltroString.Split('-');
 
-        public string FiltroString { get; set; }
-        public string CategoriaNome { get; set; }
-        public string StatusNome { get; set; }
-        public string Vencimento { get; set; }
+            // Verifica se o array tem o tamanho esperado de 3 elementos
+            CategoriaNome = filtros.Length > 0 ? filtros[0] : "todos";
+            StatusNome = filtros.Length > 1 ? filtros[1] : "todos";
+            Vencimento = filtros.Length > 2 ? filtros[2] : "todos";
+        }
+
+        public string FiltroString { get; }
+        public string CategoriaNome { get; }
+        public string StatusNome { get; }
+        public string Vencimento { get; }
+
+        public bool TemCategoria => !string.Equals(CategoriaNome, "todos", StringComparison.OrdinalIgnoreCase);
+        public bool TemStatus => !string.Equals(StatusNome, "todos", StringComparison.OrdinalIgnoreCase);
+        public bool TemVencimento => !string.Equals(Vencimento, "todos", StringComparison.OrdinalIgnoreCase);
+
+
+        public static Dictionary<string, string> VencimentoValoresFiltro => new Dictionary<string, string>
+        {
+            {"futuro", "futuro" },
+            {"passado", "passado" },
+            {"hoje", "hoje" }
+        };
+
+        //Dessa forma é menos perfomatico: public bool EFuturo => Vencimento.ToLower() == "passado";  
+        //Dessa forma abaixo é melhor, eu não precisso criar um novo obj em memória para comparar o valores
+        public bool EFuturo => !string.Equals(Vencimento, "futuro", StringComparison.OrdinalIgnoreCase);
+        public bool EPassado => !string.Equals(Vencimento, "passado", StringComparison.OrdinalIgnoreCase);
+        public bool EHoje => !string.Equals(Vencimento, "hoje", StringComparison.OrdinalIgnoreCase);
+
+
     }
 }
